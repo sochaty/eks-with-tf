9523c03afa907f9be8ad3b37ddadbe5b80436b51
Sourish <socha@orsted.com>
2023-07-25 14:31:45 +0800

    implemented network deployment

--- terraform/modules/network/variables.tf
+++ terraform/modules/network/variables.tf
@@ -1,5 +1,21 @@
 # VPC Input Variables
 
+variable "aws_region" {
+  description = "Region Name"
+  type = string 
+  default = "us-east-1"
+}
+
+variable "business_division" {
+  description = "Team"
+  type = string 
+}
+
+variable "environment" {
+  description = "Infrastructure Environment"
+  type = string 
+}
+
 # VPC Name
 variable "vpc_name" {
   description = "VPC Name"
@@ -11,35 +27,35 @@ variable "vpc_name" {
 variable "vpc_cidr_block" {
   description = "VPC CIDR Block"
   type = string 
-  default = "10.0.0.0/16"
 }
 
 # VPC Availability Zones
 variable "vpc_availability_zones" {
   description = "VPC Availability Zones"
   type = list(string)
-  default = ["us-east-1a", "us-east-1b"]
 }
 
 # VPC Public Subnets
 variable "vpc_public_subnets" {
   description = "VPC Public Subnets"
   type = list(string)
-  default = ["10.0.101.0/24", "10.0.102.0/24"]
 }
 
 # VPC Private Subnets
 variable "vpc_private_subnets" {
   description = "VPC Private Subnets"
   type = list(string)
-  default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 # VPC Database Subnets
 variable "vpc_database_subnets" {
   description = "VPC Database Subnets"
   type = list(string)
-  default = ["10.0.151.0/24", "10.0.152.0/24"]
+}
+
+variable "zones" {
+  type        = list(string)
+  description = "Availability zones for subnet deployment"
 }
 
 # VPC Create Database Subnet Group (True / False)
